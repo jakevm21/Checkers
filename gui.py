df9f644b28b5427834127d7be295c4e3cff2b169
jwke21 <jakevm21@gmail.com>
2022-07-19 16:34:49 -0700

    functionality to draw board and checkers

--- gui.py
+++ gui.py
@@ -1,8 +1,12 @@
 import turtle as turt
+from piece_options import PieceOptions as po
+
 NUM_SQS = 8     # The number of squares on each row
 SQ_SIZE = 50    # The size of each square in the checkerboard
 SQ_COLOR = "light gray"
 OUTLINE_COLOR = "black"
+BG_COLOR = "white"
+CROWN_POS = 7
 
 class GUI:
     def __init__(self, num_sqs=NUM_SQS, sq_size=SQ_SIZE) -> None:
@@ -10,15 +14,18 @@ class GUI:
         self.sq_size = sq_size
         self.brd_size = num_sqs * sq_size         # Size of the board
         self.wndw_size = self.brd_size + sq_size  # The extra + SQUARE is the margin
-        self.def_pos = -self.brd_size / 2 - 1     # Bottom left corner of board
+        self.def_pos = (-self.brd_size >> 1) - 1    # Bottom left corner of board
+        self.piece_pos = self.sq_size >> 1        # Center of a square
+        self.piece_size = self.sq_size >> 1
+        self.crown_pos = CROWN_POS                # Position of crown on piece
+        self.crow_size = self.piece_size * 0.7    # Size of crown on piece
 
         self._screen_setup()
         self.pen = self._initialize_turt()        # Variable that will do the drawing
         self.draw_board()
-        
+        self._draw_all_squares()
 
     def _screen_setup(self) -> None:
-        BG_COLOR = "white"
         # Create the UI window. Size is width of the board plus a little margin
         turt.setup(self.wndw_size, self.wndw_size)
 
@@ -43,7 +50,7 @@ class GUI:
                 Nothing. Draws the board in the graphics window.
         '''
         # Board outline is black, filler is white
-        self.pen.color("black", "white")
+        self.pen.color(OUTLINE_COLOR, BG_COLOR)
         # Outline of checkerboard
         self.pen.setposition(self.def_pos, self.def_pos)
         self._draw_square(self.brd_size)
@@ -69,5 +76,26 @@ class GUI:
             self.pen.end_fill()
             self.pen.penup()
 
-    def draw_checker(self, col: int, row: int, color: str, rank: int) -> None:
-        pass
+    def _draw_circle(self, size):
+        self.pen.pendown()
+        self.pen.begin_fill()
+        self.pen.circle(size)
+        self.pen.end_fill()
+        self.pen.penup()
+
+    def draw_checker(self, row: int, col: int, color: po, rank: po) -> None:
+        # Set the turtle in position to draw a checker
+        x_pos = self.def_pos + (self.sq_size * col) + self.piece_pos
+        y_pos = self.def_pos + (self.sq_size * row)
+        self.pen.setposition(x_pos, y_pos)
+
+        if color == po.blk:
+            self.pen.color("black")
+        else:
+            self.pen.color("red")
+
+        self._draw_circle(self.piece_size)
+        # if piece is king draw crown
+        if rank == 0:
+            # TODO: Draw crown
+            pass
